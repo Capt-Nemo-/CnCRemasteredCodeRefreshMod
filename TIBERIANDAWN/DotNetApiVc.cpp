cb87883cbf2cd43421d6ca1d7ff5095e3096a203
Nemo <Capt-Nemo-@users.noreply.github.com>
2020-06-10 01:12:15 +0200

    bugfix

--- TIBERIANDAWN/DotNetApiVc.cpp
+++ TIBERIANDAWN/DotNetApiVc.cpp
@@ -17,34 +17,34 @@ public:
 	virtual void SaveGame(String^ filename, String^ description)
 	{
 		const pin_ptr<String^> fnamePtr = &filename, descPtr = &description;
-
+		
 		if (!Save_Game(reinterpret_cast<char*>(fnamePtr), reinterpret_cast<char*>(descPtr)))
 			throw gcnew Exception(gcnew String("Save game procedure failed."));
 	}
-
+	
 	virtual void LoadGame(String^ filename)
 	{
 		const pin_ptr<String^> fnamePtr = &filename;
-
+		
 		if (!Load_Game(reinterpret_cast<char*>(fnamePtr)))
 			throw gcnew Exception(gcnew String("Load game procedure failed."));
 	}
-
-	virtual void ShowQuickMessage(String^ text, CnCTextColor color, const int timeoutMs)
+	
+	virtual void ShowQuickMessage(String^ text, CnCTextColor color, int timeoutMs)
 	{
 		const auto ticks = timeoutMs == Threading::Timeout::Infinite
 			                   ? -1
 			                   : (timeoutMs / (1000 / DotNetApiVc::Global_Ticks_Per_Sec));
-
+		
 		auto intPtr = Marshal::StringToHGlobalAnsi(text);
 		const auto ptr = static_cast<char*>(static_cast<void*>(intPtr));
-
+		
 		Messages.Add_Message(ptr, static_cast<int>(color), TPF_6PT_GRAD | TPF_USE_GRAD_PAL | TPF_FULLSHADOW, ticks, 0,
 		                     0);
-
+		
 		Marshal::FreeHGlobal(intPtr);
 	}
-
+	
 	static CnC^ Cs = nullptr;	
 };
 
