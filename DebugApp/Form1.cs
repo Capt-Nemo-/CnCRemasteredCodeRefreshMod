9679246c145640db67ee4b940b506859af4ed56e
CptNemo <tabt@seznam.cz>
2020-06-10 18:50:46 +0200

    no!

---
+++ DebugApp/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace DebugApp
+{
+    public partial class Form1 : Form
+    {
+        //[return: MarshalAs(UnmanagedType.)]
+
+        [DllImport("TiberianDawn.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int CNC_Version(uint versionIn);
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            int ver = CNC_Version(45);
+        }
+    }
+}
